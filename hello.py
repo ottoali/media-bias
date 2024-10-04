4e24388f46e4414ea5c66bbf4e8cc4a2eb37f8dd
ottoali <ottoali99@gmail.com>
2024-10-04 11:26:30 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -16,23 +16,20 @@ GITHUB_TOKEN = st.secrets["key"]  # Access token from Streamlit secrets
 
 # Function to fetch and combine CSVs from GitHub
 def load_data_from_github(file_list):
-combined_data = pd.DataFrame()  # Initialize an empty DataFrame
-
-for file_name in file_list:
-url = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{REPO_NAME}/main/{file_name}"
-headers = {'Authorization': f'token {GITHUB_TOKEN}'}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-csv_data = pd.read_csv(StringIO(response.text))
-combined_data = pd.concat([combined_data, csv_data], ignore_index=True)  # Combine DataFrames
-else:
-st.error(f"Failed to load data from {file_name}.")
-
-return combined_data
+  combined_data = pd.DataFrame()  # Initialize an empty DataFrame
+  
+  for file_name in file_list:
+  url = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{REPO_NAME}/main/{file_name}"
+  headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+  
+  response = requests.get(url, headers=headers)
+  
+  if response.status_code == 200:
+  csv_data = pd.read_csv(StringIO(response.text))
+  combined_data = pd.concat([combined_data, csv_data], ignore_index=True)  # Combine DataFrames
+  else:
+  st.error(f"Failed to load data from {file_name}.")
+  
+  return combined_data
 
 data = load_data_from_github(CSV_FILES)
-
-# Display the DataFrame if loaded successfully
-if not data.empty:
