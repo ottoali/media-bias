540d9f23924fcc91d8506437e0f9b872a3612bfb
ottoali <ottoali99@gmail.com>
2024-10-04 13:02:14 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,7 +10,9 @@ pd.set_option("styler.render.max_elements", 1000)
 
 GITHUB_USERNAME = "ottoali"  # Replace with your GitHub username
 REPO_NAME = "data"              # Replace with your repo name
-CSV_FILES = []  # List of CSV files
+CSV_FILES = ["lines_output_part_1.csv","lines_output_part_2.csv","lines_output_part_3.csv","lines_output_part_4.csv",
+             "lines_output_part_5.csv","lines_output_part_6.csv","lines_output_part_7.csv","lines_output_part_8.csv",
+            "lines_output_part_9.csv","lines_output_part_10.csv"]  # List of CSV files
 GITHUB_TOKEN = st.secrets["key"]  # Access token from Streamlit secrets
 
 # Function to fetch and combine CSVs from GitHub
